cbf6fc306284ec26ced3e9f8a15002cc52c5847a
IzaakWN <iwn_@hotmail.com>
2017-06-11 14:34:54 +0200

    New\! Improved\!

--- TauTauResonancesEM/run.sh
+++ TauTauResonancesEM/run.sh
@@ -1,12 +1,21 @@
 #! /bin/bash
 
+FILES=(
+    Signal.xml
+#     DYJets_M-10to50.xml
+#     DYJets_M-50.xml
+#     TT_TuneCUETP8M1.xml
+#     WJetsToLNu.xml
+#     SingleMuon.xml
+#     SingleMuon_G.xml
+#     SingleMuon_H_v3.xml
+#     SingleMuon_H_v2.xml
+)
+
+printf "\n\n\n\n\n\n\n\n"
 if make distclean && make; then
-  #sframe_main config/WJetsToLNu.xml
-  sframe_main config/DYJets_M-10to50.xml
-  #sframe_main config/DYJets_M-50.xml
-  #sframe_main config/DYJets_M-50_tes.xml
-  #sframe_main config/Signal.xml
-  #sframe_main config/TT_TuneCUETP8M1.xml
-  #sframe_main config/SingleMuon.xml      #| tee SingleMuon.log
-  #sframe_main config/SUSYGluGluToHToTauTau_M-160_TuneCUETP8M1.xml
+    for f in ${FILES[@]}; do
+        printf "\n\n\n>>> %s%s%s\n\n" "$(tput bold)$(tput setaf 2)" $f "$(tput sgr0)"
+        sframe_main config/$f
+    done
 fi
