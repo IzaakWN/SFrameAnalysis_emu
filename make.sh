55c0b71f6e928dbf58682c8631f48da8e666d9ab
IzaakWN <iwn_@hotmail.com>
2017-06-12 15:50:30 +0200

    added JetCorrectionTool

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-for dir in BTaggingTools GoodRunsLists LepEff2016 NtupleVariables PileupReweightingTool SFrame SVFitTools TauTauResonances RecoilCorrections
+for dir in BTaggingTools GoodRunsLists LepEff2016 NtupleVariables PileupReweightingTool SFrame SVFitTools TauTauResonancesEM RecoilCorrections JetCorrectionTool
 do
     echo
     echo $dir
