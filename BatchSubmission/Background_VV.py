68fd8124a51ddb9ffda3a2fa6f87dac99d3fee92
IzaakWN <iwn_@hotmail.com>
2017-06-16 10:00:26 +0200

    added

--- BatchSubmission/Background_VV.py
+++ BatchSubmission/Background_VV.py
@@ -1,36 +1,22 @@
 path2xml="$SFRAME_DIR/../BatchSubmission/xmls_postMoriond_T2"
 path2tmp="$SFRAME_DIR/../AnalysisTemp"
-jobName="ZZ"
+jobName="VV"
 outDir="/scratch/$USER/SFrameAnalysis/AnalysisOutputEM/"+jobName
 cycleName="TauTauAnalysis"
 nEventsMax=-1
 #nEventsMax=200
 nProcesses=1
-nFiles=2
-hCPU="05:00:00"
+nFiles=1
+hCPU="07:00:00"
 hVMEM="5000M"
 postFix="_Moriond"
 dataSets = [
     
-   [ "ZZTo2L2Q_13TeV_nlo",
-    [   "ZZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8_0000.xml", ]],
-        
- # [ "ZZTo2Q2Nu_13TeV_nlo",
- #  [   "ZZTo2Q2Nu_13TeV_amcatnloFXFX_madspin_pythia8_V2_0000_0.xml",
- #      "ZZTo2Q2Nu_13TeV_amcatnloFXFX_madspin_pythia8_V2_0000_1.xml", ]],
+   [ "VVTo2L2Nu_13TeV_nlo",
+    [   "VVTo2L2Nu_13TeV_amcatnloFXFX_madspin_pythia8_0000.xml", ]],
     
- # [ "ZZTo4Q_13TeV_nlo",
- #  [   "ZZTo4Q_13TeV_amcatnloFXFX_madspin_pythia8_0000_0.xml",
- #      "ZZTo4Q_13TeV_amcatnloFXFX_madspin_pythia8_0000_1.xml", ]],
-        
-   [ "ZZTo4L_13TeV_nlo",
-    [   "ZZTo4L_13TeV-amcatnloFXFX-pythia8_ext_0000.xml", ]],
-        
- # [ "ZZTo4L_13TeV_powheg_pythia8",
- #  [   "ZZTo4L_13TeV_powheg_pythia8_0000.xml", ]],
-                
             ]
-            
+
 
 userItems = [ 
                 ["IsData","false"],
